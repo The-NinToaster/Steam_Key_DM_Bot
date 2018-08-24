04d1b3d866535b9f36f77bd9bc52318b46c7617b
Nikki Babaii <nikki.babaii@otherocean.com>
2018-08-24 15:51:11 -0300

    verification woes begin soon

--- bot_new.js
+++ bot_new.js
@@ -1,4 +1,4 @@
-
+const config = require ("./config.json");
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
@@ -23,14 +23,33 @@ client.on('message', message =>
 	if (message.channel.type === "text")
 	{	
 	console.log(message.content);
-	//if (message.content == '!verify')
-	//message.channel.send('This works!')
-
 	messageArray = message.content.split (" "); 
 	command = messageArray[0]; 
 	verification = messageArray[1];
 	username = message.member.user.tag; 
-	console.log(message.member.user.tag); 
+	console.log(username); 
+
+	if (typeof verification != 'undefined' && verification)
+	{	
+		charCount = verification.length;
+		if (charCount !=32)
+		{
+			message.reply("Hey that doesn't seem quite right :( ... are you sure your verification code is 32 characters long? Try again or Contact a Developer :)"); 
+			return; 
+		}
+		else 
+		{
+			if (!command.startsWith(config.prefix)) return; 
+			if (command === `${config.prefix}verify`) 
+			{
+				message.reply("hey this works!");
+			}
+		}
+
+	}
+	else 
+	message.reply ("Whoops! Looks like you're lacking a verification code. The Dev Team has sent you one in your email, make sure you enter that next time :)"); 
+		
 	}	
 	return; 
 }); 

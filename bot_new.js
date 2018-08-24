222dbf603c23e8a30a51fa48ebad9154ef559289
Nikki Babaii <nikki.babaii@otherocean.com>
2018-08-24 13:55:52 -0300

    New bot file bot_new.js to test remote hosting connections through AWS, modified config file with no hierarchy permissions

---
+++ bot_new.js
@@ -0,0 +1,28 @@
+//const config = require("./config.json");
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+//fill in mysql shit later honestly... 
+
+//start up 
+client.on('ready', async () =>
+{
+	console.log('Ready!'); 
+});
+
+client.on('message', async message => 
+{
+	//Reutrn if authored by bot 
+	if (message.author.bot) return; 
+
+	if (message.channel.type === text)
+	{	
+	console.log(message.content);
+	if (message.content == '!verify' )
+	{message.channel.send('This works!');
+	
+	}
+	
+}); 
+
+client.login('NDgxODkzOTgzMTYxOTQyMDM2.Dl8-2A.luFbY4xHcUI9G0ravu7WfHe0vhI');
